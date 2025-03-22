518cbdfac98cda1ca40042c8b98eb6e38fba5c07
Ebraam Sobhy <ibraam.sobhy2003@gmail.com>
2025-03-22 21:37:30 +0200

    new changes

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,22 +8,28 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from './logo.jpg';
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <Navbar expand="lg" className="bg-white">
             <Container className="justify-content-between">
-                <Navbar.Brand as={Link} to="/" target="_blank">
+                <Navbar.Brand as={Link} to="/">
                     <img className='logo' src={logo} alt="Logo" />
                 </Navbar.Brand>
 
                 {/* Hamburger Menu for small screens */}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" color='black' />
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    className={`custom-toggle ${isOpen ? "rotated" : ""}`}
+                    onClick={() => setIsOpen(!isOpen)}
+                />
 
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link as={Link} to="/" target="_blank">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/about" target="_blank">About</Nav.Link>
-                        <Nav.Link as={Link} to="/ref-video" target="_blank">Ref Video</Nav.Link>
-                        <Nav.Link as={Link} to="/final-exam" target="_blank">Final Exam</Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/ref-video">Ref Video</Nav.Link>
+                        <Nav.Link as={Link} to="/final-exam">Final Exam</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
